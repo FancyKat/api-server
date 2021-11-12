83c0fa2a4512fce941e0608d6aca06b25fdc2e27
FancyKat <fancykatbase@gmail.com>
2021-11-11 16:57:45 -0800

    Setting Up Template

---
+++ src/pages/music.js
@@ -0,0 +1,322 @@
+import React from "react";
+
+const HomeComponent = () => (
+  <svg viewBox="0 0 1920 3163">
+    <defs>
+      <style>
+        {
+          ".a{clip-path:url(#l);}.b,.c,.f,.j{fill:#3f0f00;}.d,.i,.k,.l,.v,.w,.x{fill:#fff;}.d{font-size:33px;font-family:SegoeUI-Bold, Segoe UI;font-weight:700;}.e{fill:url(#a);}.f{font-size:104px;}.f,.k,.l,.v,.w,.x{font-family:Trailmade-Regular, Trailmade;}.g{fill:url(#b);}.ab,.h{fill:none;}.h{stroke:#fff;stroke-width:16px;}.j,.m,.o,.q,.r{stroke:#707070;}.k{font-size:126px;}.l{font-size:49px;}.m{fill:url(#c);}.n{stroke:#ba7e4e;fill:url(#d);}.n,.o,.p,.q,.r{stroke-width:10px;}.o{fill:url(#e);}.p{stroke:#131313;fill:url(#f);}.q{fill:url(#g);}.r{fill:url(#h);}.s{fill:url(#i);}.t{fill:rgba(255,255,255,0);stroke:#4b5b48;}.t,.u{stroke-width:20px;}.u{stroke:#844f45;fill:url(#j);}.v{font-size:130px;}.w,.x{font-size:60px;}.x{stroke:#3f0f00;}.aa,.ac,.y,.z{stroke:none;}.z{fill:#707070;}.aa{fill:#fff;}.ac{fill:#844f45;}.ad{fill:#c9c9c9;}"
+        }
+      </style>
+      <pattern id="a" width={1} height={1} viewBox="0 462.951 1920 697">
+        <image
+          preserveAspectRatio="xMidYMid slice"
+          width={1920}
+          height={1441.778}
+          xlinkHref="ComponentTMP_0-image.jpg"
+        />
+      </pattern>
+      <linearGradient
+        id="b"
+        x1={0.718}
+        y1={0.517}
+        x2={0.288}
+        y2={1}
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset={0} stopColor="#4b5b48" />
+        <stop offset={1} stopColor="#0f120e" />
+      </linearGradient>
+      <pattern
+        id="c"
+        preserveAspectRatio="xMidYMid slice"
+        width="100%"
+        height="100%"
+        viewBox="0 0 1080 720"
+      >
+        <image
+          width={1080}
+          height={720}
+          xlinkHref="ComponentTMP_0-image2.jpg"
+        />
+      </pattern>
+      <pattern
+        id="d"
+        preserveAspectRatio="xMidYMid slice"
+        width="100%"
+        height="100%"
+        viewBox="0 0 1080 730"
+      >
+        <image
+          width={1080}
+          height={730}
+          xlinkHref="ComponentTMP_0-image3.jpg"
+        />
+      </pattern>
+      <pattern
+        id="e"
+        preserveAspectRatio="xMidYMid slice"
+        width="100%"
+        height="100%"
+        viewBox="0 0 400 300"
+      >
+        <image width={400} height={300} xlinkHref="ComponentTMP_0-image4.jpg" />
+      </pattern>
+      <pattern
+        id="f"
+        preserveAspectRatio="xMidYMid slice"
+        width="100%"
+        height="100%"
+        viewBox="0 0 400 267"
+      >
+        <image width={400} height={267} xlinkHref="ComponentTMP_0-image5.jpg" />
+      </pattern>
+      <pattern
+        id="g"
+        preserveAspectRatio="xMidYMid slice"
+        width="100%"
+        height="100%"
+        viewBox="0 0 400 232"
+      >
+        <image width={400} height={232} xlinkHref="ComponentTMP_0-image6.jpg" />
+      </pattern>
+      <pattern
+        id="h"
+        preserveAspectRatio="xMidYMid slice"
+        width="100%"
+        height="100%"
+        viewBox="0 0 400 266"
+      >
+        <image width={400} height={266} xlinkHref="ComponentTMP_0-image7.jpg" />
+      </pattern>
+      <linearGradient
+        id="i"
+        x1={0.676}
+        y1={0.392}
+        x2={0.429}
+        y2={0.832}
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset={0} stopColor="#844f45" />
+        <stop offset={1} stopColor="#422823" />
+      </linearGradient>
+      <pattern
+        id="j"
+        preserveAspectRatio="xMidYMid slice"
+        width="100%"
+        height="100%"
+        viewBox="0 0 400 549"
+      >
+        <image width={400} height={549} xlinkHref="ComponentTMP_0-image8.jpg" />
+      </pattern>
+      <clipPath id="l">
+        <rect width={1920} height={3163} />
+      </clipPath>
+    </defs>
+    <g id="k" className="a">
+      <rect className="ad" width={1920} height={3163} />
+      <g className="b">
+        <path
+          className="y"
+          d="M 1919.5 74.5 L 0.5 74.5 L 0.5 0.5 L 1919.5 0.5 L 1919.5 74.5 Z"
+        />
+        <path
+          className="z"
+          d="M 1 1 L 1 74 L 1919 74 L 1919 1 L 1 1 M 0 0 L 1920 0 L 1920 75 L 0 75 L 0 0 Z"
+        />
+      </g>
+      <g transform="translate(968)">
+        <rect className="c" width={198} height={75} />
+        <text className="d" transform="translate(52 52)">
+          <tspan x={0} y={0}>
+            {"Home"}
+          </tspan>
+        </text>
+      </g>
+      <g transform="translate(1173)">
+        <rect className="c" width={198} height={75} />
+        <text className="d" transform="translate(55 52)">
+          <tspan x={0} y={0}>
+            {"About"}
+          </tspan>
+        </text>
+      </g>
+      <g transform="translate(1377)">
+        <path className="c" d="M0,0H198V75H0Z" />
+        <text className="d" transform="translate(38 52)">
+          <tspan x={0} y={0}>
+            {"Sign Up"}
+          </tspan>
+        </text>
+      </g>
+      <g transform="translate(1582)">
+        <rect className="c" width={198} height={75} />
+        <text className="d" transform="translate(56 52)">
+          <tspan x={0} y={0}>
+            {"Login"}
+          </tspan>
+        </text>
+      </g>
+      <text className="d" transform="translate(140 52)">
+        <tspan x={0} y={0}>
+          {"Logo Here"}
+        </tspan>
+      </text>
+      <rect
+        className="e"
+        width={1920}
+        height={697}
+        transform="translate(0 75)"
+      />
+      <text className="f" transform="translate(559.371 464.244) rotate(-15)">
+        <tspan x={0} y={0}>
+          {"Illegal Enlightenment Productions"}
+        </tspan>
+      </text>
+      <rect
+        className="g"
+        width={1920}
+        height={697}
+        transform="translate(0 772)"
+      />
+      <g transform="translate(954.587 830.903)">
+        <path
+          className="y"
+          d="M 780.468505859375 546.4115600585938 L 75.06060791015625 546.4115600585938 C 65.74893951416016 546.4115600585938 56.69338607788086 543.83154296875 48.1453857421875 538.7432250976562 C 43.98538589477539 536.2669067382812 39.96438598632812 533.1936645507812 36.19427490234375 529.6090087890625 C 32.42893981933594 526.0288696289062 28.88238525390625 521.9092407226562 25.65305137634277 517.3641967773438 C 22.40394020080566 512.7915649414062 19.45094108581543 507.7532348632812 16.87605094909668 502.3890991210938 C 14.26582908630371 496.9515686035156 12.02249622344971 491.1336669921875 10.20827388763428 485.0971069335938 C 6.420607089996338 472.4937744140625 4.500051498413086 459.0950012207031 4.500051498413086 445.2728881835938 L 4.500051498413086 105.6387786865234 C 4.500051498413086 91.81666564941406 6.420607089996338 78.41777801513672 10.20827388763428 65.81455993652344 C 12.02249622344971 59.77800369262695 14.26582908630371 53.96011352539062 16.87605094909668 48.52255630493164 C 19.45094108581543 43.158447265625 22.40394020080566 38.12011337280273 25.65305137634277 33.54733657836914 C 28.88238525390625 29.0024471282959 32.42893981933594 24.88266944885254 36.19427490234375 21.30266952514648 C 39.96438598632812 17.71789169311523 43.98538589477539 14.64478015899658 48.1453857421875 12.16833591461182 C 56.69338607788086 7.080002307891846 65.74893951416016 4.500002384185791 75.06060791015625 4.500002384185791 L 780.468505859375 4.500002384185791 C 789.780029296875 4.500002384185791 798.8356323242188 7.080002307891846 807.3836059570312 12.16833591461182 C 811.5435791015625 14.64478015899658 815.5646362304688 17.71789169311523 819.334716796875 21.30266952514648 C 823.1000366210938 24.88266944885254 826.6466064453125 29.0024471282959 829.8759155273438 33.54733657836914 C 833.1250610351562 38.12011337280273 836.0781860351562 43.158447265625 838.653076171875 48.52255630493164 C 841.2632446289062 53.96011352539062 843.506591796875 59.77800369262695 845.32080078125 65.81455993652344 C 849.1085205078125 78.41789245605469 851.029052734375 91.81666564941406 851.029052734375 105.6387786865234 L 851.029052734375 445.2728881835938 C 851.029052734375 459.0950012207031 849.1085205078125 472.4937744140625 845.32080078125 485.0971069335938 C 843.506591796875 491.1336669921875 841.2632446289062 496.9515686035156 838.653076171875 502.3890991210938 C 836.0781860351562 507.7532348632812 833.1250610351562 512.7915649414062 829.8759155273438 517.3641967773438 C 826.6466064453125 521.9092407226562 823.1000366210938 526.0288696289062 819.334716796875 529.6090087890625 C 815.5646362304688 533.1936645507812 811.543701171875 536.2669067382812 807.3836059570312 538.7432250976562 C 798.835693359375 543.83154296875 789.780029296875 546.4115600585938 780.468505859375 546.4115600585938 Z"
+        />
+        <path
+          className="aa"
+          d="M 75.06048583984375 9 C 38.6346435546875 9 9 52.35202026367188 9 105.6387329101562 L 9 445.2728271484375 C 9 498.5595397949219 38.6346435546875 541.9115600585938 75.06048583984375 541.9115600585938 L 780.4683837890625 541.9115600585938 C 816.894287109375 541.9115600585938 846.5289916992188 498.5595397949219 846.5289916992188 445.2728271484375 L 846.5289916992188 105.6387329101562 C 846.5289916992188 52.35202026367188 816.894287109375 9 780.4683837890625 9 L 75.06048583984375 9 M 75.06048583984375 0 L 780.4683837890625 0 C 821.923095703125 0 855.5289916992188 47.2960205078125 855.5289916992188 105.6387329101562 L 855.5289916992188 445.2728271484375 C 855.5289916992188 503.6155395507812 821.923095703125 550.9115600585938 780.4683837890625 550.9115600585938 L 75.06048583984375 550.9115600585938 C 33.60577392578125 550.9115600585938 0 503.6155395507812 0 445.2728271484375 L 0 105.6387329101562 C 0 47.2960205078125 33.60577392578125 0 75.06048583984375 0 Z"
+        />
+      </g>
+      <g className="h" transform="translate(1254 976)">
+        <ellipse className="y" cx={128.5} cy={130.5} rx={128.5} ry={130.5} />
+        <ellipse className="ab" cx={128.5} cy={130.5} rx={120.5} ry={122.5} />
+      </g>
+      <g className="i" transform="translate(1467 1033) rotate(90)">
+        <path
+          className="y"
+          d="M 147.146484375 131.5000305175781 L 0.8535111546516418 131.5000305175781 L 74 1.022487163543701 L 147.146484375 131.5000305175781 Z"
+        />
+        <path
+          className="z"
+          d="M 74 2.044952392578125 L 1.707015991210938 131 L 146.2929840087891 131 L 74 2.044952392578125 M 74 -1.52587890625e-05 L 148 132 L 0 132 L 74 -1.52587890625e-05 Z"
+        />
+      </g>
+      <g className="j" transform="translate(120 728)">
+        <rect className="y" width={775} height={252} rx={126} />
+        <rect
+          className="ab"
+          x={0.5}
+          y={0.5}
+          width={774}
+          height={251}
+          rx={125.5}
+        />
+      </g>
+      <text className="k" transform="translate(314 894)">
+        <tspan x={0} y={0}>
+          {"Video Designs"}
+        </tspan>
+      </text>
+      <text className="l" transform="translate(146 1033)">
+        <tspan x={0} y={48}>
+          {"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam "}
+        </tspan>
+        <tspan x={0} y={112}>
+          {"nonumy eirmod tempor invidunt ut labore et dolore magna "}
+        </tspan>
+        <tspan x={0} y={176}>
+          {"aliquyam erat, sed diam voluptua. At vero eos et accusam et justo "}
+        </tspan>
+        <tspan x={0} y={240}>
+          {"duo dolores et ea rebum. Stet clita kasd gubergren, no sea "}
+        </tspan>
+        <tspan x={0} y={304}>
+          {"takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum"}
+        </tspan>
+      </text>
+      <g className="m" transform="translate(0 1469)">
+        <rect className="y" width={1920} height={773} />
+        <rect className="ab" x={0.5} y={0.5} width={1919} height={772} />
+      </g>
+      <g className="n" transform="translate(330 1661)">
+        <circle className="y" cx={75} cy={75} r={75} />
+        <circle className="ab" cx={75} cy={75} r={70} />
+      </g>
+      <g className="o" transform="translate(1290 1955)">
+        <circle className="y" cx={75} cy={75} r={75} />
+        <circle className="ab" cx={75} cy={75} r={70} />
+      </g>
+      <g className="p" transform="translate(885 2058)">
+        <circle className="y" cx={75} cy={75} r={75} />
+        <circle className="ab" cx={75} cy={75} r={70} />
+      </g>
+      <g className="q" transform="translate(480 1955)">
+        <circle className="y" cx={75} cy={75} r={75} />
+        <circle className="ab" cx={75} cy={75} r={70} />
+      </g>
+      <g className="r" transform="translate(1440 1661)">
+        <circle className="y" cx={75} cy={75} r={75} />
+        <circle className="ab" cx={75} cy={75} r={70} />
+      </g>
+      <g className="s" transform="translate(0 2242)">
+        <path
+          className="y"
+          d="M 1919.5 729.5 L 0.5 729.5 L 0.5 0.5 L 1919.5 0.5 L 1919.5 729.5 Z"
+        />
+        <path
+          className="ac"
+          d="M 1 1 L 1 729 L 1919 729 L 1919 1 L 1 1 M 0 0 L 1920 0 L 1920 730 L 0 730 L 0 0 Z"
+        />
+      </g>
+      <g className="t" transform="translate(330 2650)">
+        <rect className="y" width={300} height={300} rx={102} />
+        <rect className="ab" x={10} y={10} width={280} height={280} rx={92} />
+      </g>
+      <g className="t" transform="translate(114 2324)">
+        <rect className="y" width={300} height={300} rx={102} />
+        <rect className="ab" x={10} y={10} width={280} height={280} rx={92} />
+      </g>
+      <g className="u" transform="translate(180 2392)">
+        <rect className="y" width={389} height={493} rx={92} />
+        <rect className="ab" x={10} y={10} width={369} height={473} rx={82} />
+      </g>
+      <text className="v" transform="translate(727 2477)">
+        <tspan x={0} y={0}>
+          {"About Artist Edifice"}
+        </tspan>
+      </text>
+      <text className="w" transform="translate(727 2510)">
+        <tspan x={0} y={58}>
+          {
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy "
+          }
+        </tspan>
+        <tspan x={0} y={136}>
+          {
+            "eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam "
+          }
+        </tspan>
+        <tspan x={0} y={214}>
+          {"voluptua. At vero eos et"}
+        </tspan>
+      </text>
+      <g className="b" transform="translate(0 2972)">
+        <path
+          className="y"
+          d="M 1919.5 190.5 L 0.5 190.5 L 0.5 0.5 L 1919.5 0.5 L 1919.5 190.5 Z"
+        />
+        <path
+          className="z"
+          d="M 1 1 L 1 190 L 1919 190 L 1919 1 L 1 1 M 0 0 L 1920 0 L 1920 191 L 0 191 L 0 0 Z"
+        />
+      </g>
+      <text className="x" transform="translate(807 3087)">
+        <tspan x={0} y={0}>
+          {"Copyright by FancKat"}
+        </tspan>
+      </text>
+    </g>
+  </svg>
+);
