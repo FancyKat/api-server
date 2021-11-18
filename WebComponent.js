c6571da4c46c0392317d521ee760647972c9d602
FancyKat <fancykatbase@gmail.com>
2021-11-18 14:57:39 -0800

    More Design

--- WebComponent.js
+++ WebComponent.js
@@ -1,36 +1,20 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
 import React from "react";
-import "./App.css";
-import HeroBanner from "./pages/assets/home-img/ComponentTMP_0-image.jpg";
-import AboutMe from "./pages/assets/home-img/ComponentTMP_0-image2.jpg";
-import Profile from "./pages/assets/home-img/ComponentTMP_0-image3.jpg";
-import Project_0 from "./pages/assets/home-img/ComponentTMP_0-image4.jpg";
-import Project_1 from "./pages/assets/home-img/ComponentTMP_0-image5.jpg";
-import Project_2 from "./pages/assets/home-img/ComponentTMP_0-image6.jpg";
-import Project_3 from "./pages/assets/home-img/ComponentTMP_0-image7.jpg";
-import Project_4 from "./pages/assets/home-img/ComponentTMP_0-image8.jpg";
-import Project_5 from "./pages/assets/home-img/ComponentTMP_0-image9.jpg";
-import Project_6 from "./pages/assets/home-img/ComponentTMP_0-image10.jpg";
-import Project_7 from "./pages/assets/home-img/ComponentTMP_0-image11.jpg";
-import Project_8 from "./pages/assets/home-img/ComponentTMP_0-image12.jpg";
-import Project_9 from "./pages/assets/home-img/ComponentTMP_0-image13.jpg";
-import Project_10 from "./pages/assets/home-img/ComponentTMP_0-image14.jpg";
-import Project_11 from "./pages/assets/home-img/ComponentTMP_0-image15.jpg";
-import Project_12 from "./pages/assets/home-img/ComponentTMP_0-image16.jpg";
-import Project_13 from "./pages/assets/home-img/ComponentTMP_0-image17.jpg";
-import Project_14 from "./pages/assets/home-img/ComponentTMP_0-image18.jpg";
-import Project_15 from "./pages/assets/home-img/ComponentTMP_0-image19.jpg";
-import Project_16 from "./pages/assets/home-img/ComponentTMP_0-image20.jpg";
-import Project_17 from "./pages/assets/home-img/ComponentTMP_0-image21.jpg";
-import Project_18 from "./pages/assets/home-img/ComponentTMP_0-image22.jpg";
-import Project_19 from "./pages/assets/home-img/ComponentTMP_0-image23.jpg";
 
-
-
-
-
-const App = () => (
-  <svg viewBox="0 0 1920 6194">
+const WebComponent = () => (
+  <svg width={1920} height={6194} viewBox="0 0 1920 6194">
     <defs>
+      <style>
+        {
+          ".a,.h{fill:none;}.b{clip-path:url(#ab);}.c{fill:#3f0f00;}.d{fill:url(#a);}.af,.ah,.am,.ao,.e,.m{fill:#fff;}.e{font-size:33px;font-family:SegoeUI-Bold, Segoe UI;font-weight:700;}.f{font-size:204px;}.af,.ah,.am,.ao,.f,.j,.k,.m{font-family:Trailmade-Regular, Trailmade;}.g{fill:url(#b);}.ag,.h,.i{stroke:#707070;}.h{stroke-width:30px;}.i{stroke-width:29px;fill:url(#c);}.j{font-size:117px;}.k{stroke:#000;}.ao,.k{font-size:63px;}.l{fill:url(#d);}.m{font-size:115px;}.n{clip-path:url(#e);}.aa,.ab,.ac,.o,.p,.q,.r,.s,.t,.u,.v,.w,.x,.y,.z{stroke:#7185ac;stroke-width:27px;}.o{fill:url(#f);}.p{fill:url(#g);}.q{fill:url(#h);}.r{fill:url(#i);}.s{fill:url(#j);}.t{fill:url(#k);}.u{fill:url(#l);}.v{fill:url(#m);}.w{fill:url(#n);}.x{fill:url(#o);}.y{fill:url(#p);}.z{fill:url(#q);}.aa{fill:url(#r);}.ab{fill:url(#s);}.ac{fill:url(#t);}.ad{fill:url(#u);}.ae{fill:url(#v);}.af{font-size:120px;}.ag{fill:url(#w);}.ah{font-size:77px;}.ai{fill:#7185ac;}.aj{fill:url(#x);}.ak{fill:url(#y);}.al{fill:url(#z);}.am{font-size:94px;}.an{fill:#757476;}.ap{stroke:none;}.aq{fill:#f2f2f2;}"
+        }
+      </style>
       <pattern id="a" width={1} height={1} viewBox="0 134.913 1920 752">
         <image
           preserveAspectRatio="xMidYMid slice"
@@ -74,7 +58,7 @@ const App = () => (
         height="100%"
         viewBox="0 0 400 266"
       >
-        <image width={400} height={266} xlinkHref={Project_0} />
+        <image width={400} height={266} xlinkHref="ComponentTMP_0-image4.jpg" />
       </pattern>
       <pattern
         id="g"
@@ -83,7 +67,7 @@ const App = () => (
         height="100%"
         viewBox="0 0 400 267"
       >
-        <image width={400} height={267} xlinkHref={Project_1} />
+        <image width={400} height={267} xlinkHref="ComponentTMP_0-image5.jpg" />
       </pattern>
       <pattern
         id="h"
@@ -92,7 +76,7 @@ const App = () => (
         height="100%"
         viewBox="0 0 400 600"
       >
-        <image width={400} height={600} xlinkHref={Project_2} />
+        <image width={400} height={600} xlinkHref="ComponentTMP_0-image6.jpg" />
       </pattern>
       <pattern
         id="i"
@@ -101,7 +85,7 @@ const App = () => (
         height="100%"
         viewBox="0 0 400 601"
       >
-        <image width={400} height={601} xlinkHref={Project_3} />
+        <image width={400} height={601} xlinkHref="ComponentTMP_0-image7.jpg" />
       </pattern>
       <pattern
         id="j"
@@ -110,7 +94,7 @@ const App = () => (
         height="100%"
         viewBox="0 0 400 267"
       >
-        <image width={400} height={267} xlinkHref={Project_4} />
+        <image width={400} height={267} xlinkHref="ComponentTMP_0-image8.jpg" />
       </pattern>
       <pattern
         id="k"
@@ -119,7 +103,7 @@ const App = () => (
         height="100%"
         viewBox="0 0 400 300"
       >
-        <image width={400} height={300} xlinkHref={Project_5} />
+        <image width={400} height={300} xlinkHref="ComponentTMP_0-image9.jpg" />
       </pattern>
       <pattern
         id="l"
@@ -131,7 +115,7 @@ const App = () => (
         <image
           width={400}
           height={600}
-          xlinkHref={Project_6}
+          xlinkHref="ComponentTMP_0-image10.jpg"
         />
       </pattern>
       <pattern
@@ -144,7 +128,7 @@ const App = () => (
         <image
           width={400}
           height={267}
-          xlinkHref={Project_7}
+          xlinkHref="ComponentTMP_0-image11.jpg"
         />
       </pattern>
       <pattern
@@ -157,7 +141,7 @@ const App = () => (
         <image
           width={400}
           height={267}
-          xlinkHref={Project_8}
+          xlinkHref="ComponentTMP_0-image12.jpg"
         />
       </pattern>
       <pattern
@@ -170,7 +154,7 @@ const App = () => (
         <image
           width={400}
           height={267}
-          xlinkHref={Project_9}
+          xlinkHref="ComponentTMP_0-image13.jpg"
         />
       </pattern>
       <pattern
@@ -183,7 +167,7 @@ const App = () => (
         <image
           width={400}
           height={600}
-          xlinkHref={Project_10}
+          xlinkHref="ComponentTMP_0-image14.jpg"
         />
       </pattern>
       <pattern
@@ -196,7 +180,7 @@ const App = () => (
         <image
           width={400}
           height={267}
-          xlinkHref={Project_11}
+          xlinkHref="ComponentTMP_0-image15.jpg"
         />
       </pattern>
       <pattern
@@ -209,7 +193,7 @@ const App = () => (
         <image
           width={400}
           height={300}
-          xlinkHref={Project_12}
+          xlinkHref="ComponentTMP_0-image16.jpg"
         />
       </pattern>
       <pattern
@@ -222,7 +206,7 @@ const App = () => (
         <image
           width={400}
           height={267}
-          xlinkHref={Project_13}
+          xlinkHref="ComponentTMP_0-image17.jpg"
         />
       </pattern>
       <pattern
@@ -235,7 +219,7 @@ const App = () => (
         <image
           width={400}
           height={600}
-          xlinkHref={Project_14}
+          xlinkHref="ComponentTMP_0-image18.jpg"
         />
       </pattern>
       <linearGradient
@@ -258,7 +242,7 @@ const App = () => (
         <image
           width={400}
           height={259}
-          xlinkHref={Project_15}
+          xlinkHref="ComponentTMP_0-image19.jpg"
         />
       </pattern>
       <pattern
@@ -271,7 +255,7 @@ const App = () => (
         <image
           width={400}
           height={225}
-          xlinkHref={Project_16}
+          xlinkHref="ComponentTMP_0-image20.jpg"
         />
       </pattern>
       <pattern
@@ -284,7 +268,7 @@ const App = () => (
         <image
           width={400}
           height={276}
-          xlinkHref={Project_17}
+          xlinkHref="ComponentTMP_0-image21.jpg"
         />
       </pattern>
       <pattern
@@ -297,7 +281,7 @@ const App = () => (
         <image
           width={400}
           height={300}
-          xlinkHref={Project_18}
+          xlinkHref="ComponentTMP_0-image22.jpg"
         />
       </pattern>
       <pattern
@@ -310,7 +294,7 @@ const App = () => (
         <image
           width={400}
           height={300}
-          xlinkHref={Project_19}
+          xlinkHref="ComponentTMP_0-image23.jpg"
         />
       </pattern>
       <clipPath id="ab">
@@ -716,4 +700,4 @@ const App = () => (
   </svg>
 );
 
-export default App;
+export default WebComponent;
