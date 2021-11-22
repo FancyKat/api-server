b9a28cb53745906008c1e44227ace8a3d2fc32b1
FancyKat <fancykatbase@gmail.com>
2021-11-22 14:38:19 -0800

    Scaffold Page

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import Navbar from './pages/components/navbar';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Banner from './pages/components/hero_banner';
+// import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <Navbar /> */}
-    <App />
+    <Banner />
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/components/hero_banner.jsx
@@ -0,0 +1,230 @@
+import React from 'react';
+import '../styles/hero_banner.css'
+
+function App() {
+  return <HeroBanner22 data-id="e711e7a0-ec3d-4788-a1b8-118381e963ba:an-component-instance" />;
+}
+
+export default App;
+
+function HeroBanner22() {
+  return (
+    <div class="container-center-horizontal">
+  <div class="home-1280 screen" data-id="3d301a62-468d-4116-b030-1e93d0847851">
+    <div class="navigation-bar" data-id="a433e10d-59bf-41f4-99f0-622649c5650d">
+      <a href="https://github.com/FancyKat">
+        <div class="home-button" data-id="bfe432fe-cfc3-4be0-b858-928a4569277a">
+          <div class="place segoeui-bold-white-29px" data-id="a0b63353-df4f-410c-b5fd-32b1f3e6cd69">Home</div>
+        </div></a
+      >
+      <div class="about-button" data-id="5115ef92-713e-4c42-b8e3-9b77557be5b7">
+        <div class="about segoeui-bold-white-29px" data-id="fbcf2929-d183-403b-8bd3-c41e80015ac7">About</div>
+      </div>
+      <div class="sign-up-button" data-id="e42ebcef-967c-4432-a766-775cbe940cc3">
+        <div class="sign-up segoeui-bold-white-29px" data-id="fc9a37c0-2e6e-4bb6-9da1-fc78c3b0b04a">Sign Up</div>
+      </div>
+      <div class="login-button" data-id="2011a6a3-b595-4b70-9540-b611809c0c34">
+        <div class="login segoeui-bold-white-29px" data-id="c2de969d-3313-4d6d-82ae-9a81760d3f90">Login</div>
+      </div>
+      <div class="logo-setting segoeui-bold-white-33px" data-id="76c39e91-231c-48c7-9a3e-a5f056c2aad7">Logo Here</div>
+    </div>
+    <div class="hero-banner" data-id="e711e7a0-ec3d-4788-a1b8-118381e963ba">
+      <h1 class="title trailmade-regular-normal-black-204px" data-id="8087d306-1d7e-4ca0-ae89-81641c6db6c3">
+        FancyKat Studios
+      </h1>
+    </div>
+    <div class="about-1" data-id="5742bbc6-b4d4-41f5-8201-9e85985d530f">
+      <div class="overlap-group" data-id="an|LXXk1BmR">
+        <img
+          class="path-2"
+          data-id="7afa67af-b7b0-4e49-bf26-9ad3a4b87928"
+          src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/path-2@1x.png"
+        />
+        <img
+          class="path-3"
+          data-id="3036d425-c4e3-4402-87fe-ccbb09e4134e"
+          src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/path-2@1x.png"
+        />
+        <img
+          class="rectangle-6"
+          data-id="33bc8c24-fce4-49fb-9879-e347c1316cce"
+          src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-6-2@1x.png"
+        />
+      </div>
+      <div class="text" data-id="973ef547-bbe3-415f-9829-9ec4bfceb5cc">
+        <div class="overlap-group1" data-id="an|KNaMc5IJ">
+          <div class="about-me" data-id="2e480698-d6ed-42da-95d0-9df35f68eb11">About Me</div>
+          <div class="lorem-ipsum-dolor-si" data-id="ff021974-6d74-4a7d-adbc-ea2ca7a99987">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+            dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+            clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+            consetetur sadipscing
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="overlap-group4" data-id="an|SvRm5Z9f">
+      <div class="projects" data-id="119231db-d1e8-499a-9891-8402a6032d2b">
+        <div class="overlap-group2" data-id="an|64yJbaA2">
+          <div class="projects-1 trailmade-regular-normal-white-115px" data-id="80285ab5-27d5-425e-8760-e70f25ff51a1">
+            Projects
+          </div>
+          <div class="component-14-2" data-id="870253cf-d97c-4473-ad49-c1a3410d380f">
+            <img
+              class="rectangle-18 smart-layers-pointers"
+              data-id="8301509b-62aa-4a45-83c3-e5426fc32605"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-30@1x.png"
+            />
+            <img
+              class="rectangle-18-1 smart-layers-pointers"
+              data-id="4c9152ed-0ad4-4a96-b0d7-8f71286d56e5"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-31@1x.png"
+            />
+            <img
+              class="rectangle-18-2 smart-layers-pointers"
+              data-id="bb391e7b-f6dc-45a9-bb08-0c5e6c5eee7c"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-32@1x.png"
+            />
+            <img
+              class="rectangle-18-3 smart-layers-pointers"
+              data-id="3b217a6a-9678-4099-a473-98943cf19b99"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-33@1x.png"
+            />
+            <img
+              class="rectangle-18-4 smart-layers-pointers"
+              data-id="65a7f6ac-2437-4aa2-a8fd-6758ab4191f1"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-34@1x.png"
+            />
+            <img
+              class="rectangle-18-5 smart-layers-pointers"
+              data-id="b3e5a517-7671-4fb9-95e7-0b36e4c2f252"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-35@1x.png"
+            />
+            <img
+              class="rectangle-18-6 smart-layers-pointers"
+              data-id="866b70bc-c4a2-4885-a0f6-341f37b8f06f"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-36@1x.png"
+            />
+            <img
+              class="rectangle-18-7 smart-layers-pointers"
+              data-id="97d404c4-72ce-436e-947e-09ef0966281c"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-37@1x.png"
+            />
+            <img
+              class="rectangle-18-8 smart-layers-pointers"
+              data-id="9e04c0e3-1e3c-45e2-b024-c820beb5f58d"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-38@1x.png"
+            />
+            <img
+              class="rectangle-18-9 smart-layers-pointers"
+              data-id="30a092ff-f85a-408d-98f9-207262ff60bc"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-39@1x.png"
+            />
+            <img
+              class="rectangle-18-10 smart-layers-pointers"
+              data-id="9dd32213-5f88-44f8-9a07-7191db33126d"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-40@1x.png"
+            />
+            <img
+              class="rectangle-18-11 smart-layers-pointers"
+              data-id="ecc654f5-f908-4081-8cd9-fe760b91f3ef"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-41@1x.png"
+            />
+            <img
+              class="rectangle-18-12 smart-layers-pointers"
+              data-id="dc9e40cb-cb5d-40aa-9842-7c35457f3e87"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-42@1x.png"
+            />
+            <img
+              class="rectangle-18-13 smart-layers-pointers"
+              data-id="548cf54f-afc0-49fb-a089-820d14843f40"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-43@1x.png"
+            />
+            <img
+              class="rectangle-18-14 smart-layers-pointers"
+              data-id="a87d9d79-bdd7-4c92-a2eb-29dd4989b55e"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-44@1x.png"
+            />
+            <img
+              class="rectangle-18-15 smart-layers-pointers"
+              data-id="a118af58-754d-439a-889f-c0e9817dfe84"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-30@1x.png"
+            />
+            <img
+              class="rectangle-18-16 smart-layers-pointers"
+              data-id="6d751fea-55d6-4f2a-8ebe-e8fcea59b0c7"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-31@1x.png"
+            />
+            <img
+              class="rectangle-18-17 smart-layers-pointers"
+              data-id="52a05096-3bf9-4f42-8bc7-75cc5dfd24bf"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-18-32@1x.png"
+            />
+          </div>
+        </div>
+        <div class="rectangle-19" data-id="1ccd01c7-a95c-4f28-9457-93c230555adc"></div>
+      </div>
+      <div class="resume-link" data-id="c3dae295-5f16-481b-a2fc-83149029d643">
+        <div class="component-8-3" data-id="6039911a-f0bd-4a97-9546-6a9c634b3194">
+          <img
+            class="rectangle-20"
+            data-id="900b9975-42f2-4f22-9961-84ca26c3c44e"
+            src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-20-2@1x.png"
+          />
+        </div>
+        <div class="overlap-group3" data-id="an|V15HQTEK">
+          <div class="resume trailmade-regular-normal-white-120px" data-id="010c3d70-26dd-415f-a6e4-26e07086aa42">
+            Resume
+          </div>
+          <img
+            class="rectangle-21"
+            data-id="3a3e911b-cbb3-437b-95d4-3747867439c9"
+            src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/619bdc7871fc5addefa2fa87/img/rectangle-21-2@1x.png"
+          />
+        </div>
+        <div class="text-1 trailmade-regular-normal-white-77px" data-id="97aa6877-bb22-4ceb-a979-bf3820c00474">
+          <div class="paulo-martin" data-id="f531c5bc-d600-4da1-9a04-54ea5bd2cca6">Paulo Martin</div>
+          <div class="software-developer" data-id="5a1a120a-c89f-442e-9f36-408de256dd70">Software Developer</div>
+          <div class="full-stack" data-id="f7178854-9638-4250-9a33-86ba4cda0130">Full Stack</div>
+        </div>
+      </div>
+    </div>
+    <div class="media" data-id="b06a72d1-933d-4e0f-b395-16fe915bcfd0">
+      <div class="links" data-id="39fd2d73-9f7e-4448-b440-52633697c24f">
+        <a href="https://github.com/FancyKat">
+          <div class="github" data-id="9e9136bc-ff2a-44ad-aad2-7d82df3c409e">
+            <img
+              class="ellipse"
+              data-id="4d59c81d-1047-4913-a95c-aaf8799d2a8a"
+              src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/6199859e86805d8204ccad0a/img/ellipse-1@1x.png"
+            /></div
+        ></a>
+        <div class="instagram" data-id="0790a56b-6772-42b2-a576-ffe8646dd812">
+          <img
+            class="ellipse"
+            data-id="8904f119-7b88-4f13-b9b3-b1227ae91718"
+            src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/6199859e86805d8204ccad0a/img/ellipse-2@1x.png"
+          />
+        </div>
+        <div class="linked-in" data-id="ded82f82-078b-46b7-a784-d48f5abad736">
+          <img
+            class="ellipse"
+            data-id="ff2b880b-c2ed-45ce-b4f6-489096583fc3"
+            src="https://anima-uploads.s3.amazonaws.com/projects/619984e686805d8204ccad09/releases/6199859e86805d8204ccad0a/img/ellipse-3@1x.png"
+          />
+        </div>
+      </div>
+      <div class="media-links trailmade-regular-normal-white-94px" data-id="cf9aeac9-849c-4622-a796-ed37eff50c3f">
+        Media Links
+      </div>
+    </div>
+    <div class="footer" data-id="f25b4091-190e-460a-8738-4c47854104e6">
+      <div class="fancy-kat trailmade-regular-normal-white-63px" data-id="07fba4b6-3f4c-4fcc-b8fa-11c94f421da8">
+        FancyKat
+      </div>
+    </div>
+  </div>
+</div>
+  );
+}
+
